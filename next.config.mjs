d2fa986ebee5847da9bfd77bd9391c8b14563f6c
MarquezA444 <ander210521@gmail.com>
2026-03-22 22:27:44 -0500

    fix: ultra simplified pwa config for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -2,14 +2,9 @@ import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
   dest: "public",
-  cacheOnFrontEndNav: true,
-  aggressiveFrontEndNavCaching: true,
-  reloadOnOnline: true,
   disable: process.env.NODE_ENV === "development",
-  workboxOptions: {
-    disableDevLogs: true,
-    importScripts: ["/push-notifications.js"],
-  },
+  register: true,
+  skipWaiting: true,
 });
 
 const nextConfig = {
